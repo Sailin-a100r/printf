19261d6b379ee6c39104e9b9da1f7cb8479eca09
Sailin-a100r <theinsan4444@gmail.com>
2023-03-24 20:37:00 +0000

    _printf.c

---
+++ _printf.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdarg.h>
+
+/**
+ * _printf - function that produces output according to a format
+ *
+ * @format: string of specified format
+ * @...: additional arguments
+ *
+ * Return: 0 success, -1 otherwise
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int i;
+	char c;
+	char *s;
+
+	if (format == NULL)
+		return (-1);
+	i = 0;
+	va_start(args, format);
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == 'c')
+			{
+				c = va_arg(args, int);
+				_putchar(c);
+				i++;
+				continue;
+			}
+			else if (format[i] == 's')
+			{
+				s = va_arg(args, char *);
+				_printf(s);
+				i++;
+				continue;
+			}
+			else if (format[i] == 'd')
+			{
+
+		}
+		_putchar(format[i]);
+		i++;
+	}
+
+	va_end(args);
+	return (0);
+}
