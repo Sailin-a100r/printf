4cb62ae62c0d875de706dd906772ef37c033759e
Sailin-a100r <theinsan4444@gmail.com>
2023-03-27 20:56:25 +0000

    Fixing Head

--- _printf.c
+++ _printf.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "main.h"
 
 void print_buffer(char buffer[], int *buff_ind);
@@ -65,61 +64,3 @@ void print_buffer(char buffer[], int *buff_ind)
 
 	*buff_ind = 0;
 }
-
-=======
-#include <stdlib.h>
-#include "main.h"
-#include <stdarg.h>
-/**
- * _printf - function that produces output according to a format
- *
- * @format: string with format specifiers
- *
- * Return: 0 Success, -1 otherwise
- */
-
-int _printf(const char *format, ...)
-{
-	va_list arguments;
-	int i;
-	char *string, character;
-
-	if (format == NULL)
-		return (-1);
-	va_start(arguments, format);
-	i = 0;
-	while (format[i] != '\0')
-	{
-		if (format[i] == '%')
-		{
-			if (format[i + 1] == 'c')
-			{
-				character = va_arg(arguments, int);
-				if (!character)
-				{
-				print_char(character);
-				i = i + 2;
-				continue;
-				}
-			}
-			else if (format[i + 1] == 's')
-			{
-				string = va_arg(arguments, char *);
-				if (string == NULL)
-					string = "(null)";
-				_printf(string);
-				i = i + 2;
-				continue;
-			}
-			else
-			{
-				return (-1);
-			}
-		}
-		print_char(format[i]);
-		i++;
-	}
-	va_end(arguments);
-	return (0);
-}
->>>>>>> 586c7d60e52c4964fa2af7aa7f71a84316e1cf61
